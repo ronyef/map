e8bc06e4525d8ee2397ee849f5b93ead1ae02d3c
ronyef <rony.efendy@gmail.com>
2020-05-02 14:56:06 +0700

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app>
+    <v-app-bar dark app>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Mekatronik AP</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text color="white" nuxt-link to="/">Home</v-btn>
+      <v-btn text color="white" nuxt-link to="/services">Services</v-btn>
+      <v-btn text color="white">Works</v-btn>
+      <v-btn text color="white">Products</v-btn>
+      <v-btn text color="white">Blog</v-btn>
+
+      <!-- <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu left bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>-->
+    </v-app-bar>
+    <v-content>
+      <Nuxt/>
+    </v-content>
+    <v-footer dark app absolute>
+      <span class="white--text caption">&copy; 2020 - By REA during WFH & Lockdown</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<style>
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,76 @@
+export default {
+  /*
+   ** Rendering mode
+   ** Doc: https://nuxtjs.org/api/configuration-mode
+   */
+  mode: "universal",
+
+  /*
+   ** Headers of the page
+   ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
+   */
+  head: {
+    title: " Mekatronik Andalan Prima",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "PT MAP Indonesia"
+      }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+
+  /*
+   ** Global CSS
+   ** Doc: https://nuxtjs.org/api/configuration-css
+   */
+  css: [],
+
+  /*
+   ** Plugins to load before mounting the App
+   ** Doc: https://nuxtjs.org/guide/plugins
+   */
+  plugins: [],
+
+  /*
+   ** Nuxt.js modules
+   ** Doc: https://nuxtjs.org/guide/modules
+   */
+  modules: [
+    // Doc: https://http.nuxtjs.org
+    "@nuxt/http",
+    // TODO: Remove it if you want to eject from codeSandbox
+    "./codesandbox"
+  ],
+
+  buildModules: ["@nuxtjs/vuetify"],
+  vuetify: {
+    defaultAssets: {
+      font: {
+        family: "Roboto"
+      },
+      icons: "mdi"
+    }
+  },
+
+  /*
+   ** HTTP module configuration
+   */
+  http: {
+    // See https://http.nuxtjs.org/api/#options
+  },
+
+  /*
+   ** Build configuration
+   ** Doc: https://nuxtjs.org/api/configuration-build
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+};
